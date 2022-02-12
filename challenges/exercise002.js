761be8b8935afa7d9ae528b4f87280310618926f
Divya <divyabhatt16@gmail.com>
2022-02-12 10:16:32 +0000

    Exercise 2 finished implementation and testing

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,41 @@
-function getFillings(sandwich) {
-  if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+function getFillings (sandwich) {
+  if (sandwich === undefined) throw new Error('ingredients is required')
+  return sandwich.fillings
 }
 
-function isFromManchester(person) {
-  if (person === undefined) throw new Error("person is required");
-  // Your code here!
+function isFromManchester (person) {
+  if (person === undefined) throw new Error('person is required')
+  return person.city === 'Manchester'
 }
 
-function getBusNumbers(people) {
-  if (people === undefined) throw new Error("people is required");
-  // Your code here!
+function getBusNumbers (people) {
+  if (people === undefined) throw new Error('people is required')
+  return people <= 40
+    ? 1
+    : people <= 80
+    ? 2
+    : people <= 120
+    ? 3
+    : Math.ceil(people / 40)
 }
 
-function countSheep(arr) {
-  if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+function countSheep (arr) {
+  if (arr === undefined) throw new Error('arr is required')
+  let countSheeps = 0
+  arr.forEach(element => {
+    if (element === 'sheep') countSheeps += 1
+  })
+  return countSheeps
 }
 
-function hasMPostCode(person) {
-  if (person === undefined) throw new Error("person is required");
-  // Your code here!
+function hasMPostCode (person) {
+  if (person === undefined) throw new Error('person is required')
+  return !(
+    person.address.postCode.startsWith('M', 0) &&
+    person.address.city === 'Manchester'
+  )
+    ? false
+    : true
 }
 
 module.exports = {
@@ -29,4 +44,4 @@ module.exports = {
   countSheep,
   getBusNumbers,
   hasMPostCode
-};
+}
