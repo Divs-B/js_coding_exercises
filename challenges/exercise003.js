ea81149b1f1e520c8cc39bd6d858954b8e001de3
Divya <divyabhatt16@gmail.com>
2022-02-12 23:17:26 +0000

    Exercise 3 finished implementation and testing

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,28 +1,53 @@
-function getSquares(nums) {
-  if (nums === undefined) throw new Error("nums is required");
-  // Your code here!
+function getSquares (nums) {
+  if (nums === undefined) throw new Error('nums is required')
+  return nums.map(num => num * num)
 }
 
-function camelCaseWords(words) {
-  if (words === undefined) throw new Error("words is required");
-  // Your code here!
+function camelCaseWords (words) {
+  if (words === undefined) throw new Error('words is required')
+  let camelCaseWOrdResult = words[0] //put 1st word as it is in resultText
+  words.shift() // remove that 1st word from array
+  for (let word in words) {
+    // iterate through array
+    camelCaseWOrdResult +=
+      words[word].substring(0, 1).toUpperCase() +
+      words[word].slice(1, words[word].length)
+  }
+  return camelCaseWOrdResult
 }
 
-function getTotalSubjects(people) {
-  if (people === undefined) throw new Error("people is required");
-  // Your code here!
+function getTotalSubjects (people) {
+  if (people === undefined) throw new Error('people is required')
+  let totalValue = 0
+  people.forEach(element => {
+    totalValue += element.subjects.length
+  })
+  return totalValue
 }
 
-function checkIngredients(menu, ingredient) {
-  if (menu === undefined) throw new Error("menu is required");
-  if (!ingredient) throw new Error("ingredient is required");
-  // Your code here!
+function checkIngredients (menu, ingredient) {
+  if (menu === undefined) throw new Error('menu is required')
+  if (!ingredient) throw new Error('ingredient is required')
+  let isIngredientPresent = false
+  menu.map(menuItem =>
+    menuItem.ingredients.forEach(ingredientItem => {
+      if (ingredientItem === ingredient) isIngredientPresent = true
+    })
+  )
+  return isIngredientPresent
 }
 
-function duplicateNumbers(arr1, arr2) {
-  if (arr1 === undefined) throw new Error("arr1 is required");
-  if (arr2 === undefined) throw new Error("arr2 is required");
-  // Your code here!
+function duplicateNumbers (arr1, arr2) {
+  if (arr1 === undefined) throw new Error('arr1 is required')
+  if (arr2 === undefined) throw new Error('arr2 is required')
+  let newArr = []
+  arr1.forEach(element1 => {
+    arr2.forEach(element2 => {
+      if (element1 === element2) newArr.push(element1)
+    })
+  })
+  newArr.sort()
+  return [...new Set(newArr)]
 }
 
 module.exports = {
@@ -31,4 +56,4 @@ module.exports = {
   getTotalSubjects,
   checkIngredients,
   duplicateNumbers
-};
+}
