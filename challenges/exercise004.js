85ade046416f2c356fedd09bedfcc8f60faa9ac3
Divya <divyabhatt16@gmail.com>
2022-02-13 00:36:33 +0000

    Exercise 4 finished implementation and testing

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,43 +1,54 @@
-function findSmallNums(nums) {
-  if (!nums) throw new Error("nums is required");
-  // Your code here
+function findSmallNums (nums) {
+  if (!nums) throw new Error('nums is required')
+  return nums.filter(element => element < 1)
 }
 
-function findNamesBeginningWith(names, char) {
-  if (!names) throw new Error("names is required");
-  if (!char) throw new Error("char is required");
-  // Your code here
+function findNamesBeginningWith (names, char) {
+  if (!names) throw new Error('names is required')
+  if (!char) throw new Error('char is required')
+  return names.filter(element => element.startsWith(char))
 }
 
-function findVerbs(words) {
-  if (!words) throw new Error("words is required");
-  // Your code here
+function findVerbs (words) {
+  if (!words) throw new Error('words is required')
+  return words.filter(element => element.includes('to '))
 }
 
-function getIntegers(nums) {
-  if (!nums) throw new Error("nums is required");
-  // Your code here
+function getIntegers (nums) {
+  if (!nums) throw new Error('nums is required')
+  return nums.filter(element => element % 1 == 0)
 }
 
-function getCities(users) {
-  if (!users) throw new Error("users is required");
-  // Your code here
+function getCities (users) {
+  if (!users) throw new Error('users is required')
+  return users.map(user => user.data.city.displayName)
 }
 
-function getSquareRoots(nums) {
-  if (!nums) throw new Error("nums is required");
-  // Your code here
+function getSquareRoots (nums) {
+  if (!nums) throw new Error('nums is required')
+  let sqrtArray = []
+  nums.forEach(element => {
+    let sqrtValue = Math.sqrt(element)
+    sqrtValue % 1 === 0
+      ? sqrtArray.push(sqrtValue)
+      : sqrtArray.push(Number(sqrtValue.toFixed(2)))
+  })
+  return sqrtArray
 }
 
-function findSentencesContaining(sentences, str) {
-  if (!sentences) throw new Error("sentences is required");
-  if (!str) throw new Error("str is required");
-  // Your code here
+function findSentencesContaining (sentences, str) {
+  if (!sentences) throw new Error('sentences is required')
+  if (!str) throw new Error('str is required')
+  return sentences.filter(sentence => sentence.toLowerCase().includes(str))
 }
 
-function getLongestSides(triangles) {
-  if (!triangles) throw new Error("triangles is required");
-  // Your code here
+function getLongestSides (triangles) {
+  if (!triangles) throw new Error('triangles is required')
+  let longestSideOfTraingles = []
+  triangles.forEach(triangle => {
+    longestSideOfTraingles.push(Math.max(...triangle))
+  })
+  return longestSideOfTraingles
 }
 
 module.exports = {
@@ -49,4 +60,4 @@ module.exports = {
   getSquareRoots,
   findSentencesContaining,
   getLongestSides
-};
+}
